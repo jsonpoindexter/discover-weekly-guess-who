3bf28f49f0b2baaa42bd096968874e1a7994107a
Jason Poindexter <poindexter.json@gmail.com>
2022-08-06 18:16:17 -0700

    Add prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    printWidth: 90,
+    plugins: ['prettier-plugin-svelte'],
+    semi: false,
+    svelteSortOrder: 'scripts-markup-options-styles',
+    svelteStrictMode: false,
+    svelteIndentScriptAndStyle: true,
+    trailingComma: "all"
+}
